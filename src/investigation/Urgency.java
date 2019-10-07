5228b9ffd9f01a3220157e2ee009a52f7b49fb41
iuliacalatoaie <48186989+iuliacalatoaie@users.noreply.github.com>
2019-10-07 12:24:29 +0300

    Add files via upload

---
+++ src/investigation/Urgency.java
@@ -0,0 +1,15 @@
+package investigation;
+
+/**
+ * IMMEDIATE > URGENT > LESS_URGENT > NON_URGENT
+ * NON_URGENT inseamna ca nu va intra in fluxurile de urgenta.
+ *
+ * [Parte din scheletul temei]
+ */
+public enum Urgency {
+    IMMEDIATE,
+    URGENT,
+    LESS_URGENT,
+    NON_URGENT,
+    NOT_DETERMINED,
+}
